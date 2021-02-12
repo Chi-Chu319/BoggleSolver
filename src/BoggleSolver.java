fd1dd0240625b2d354e9f45f4cbf6ce902d18c45
Chi-Chu319 <chi0chu319@gmail.com>
2021-02-13 00:25:53 +0800

    Switching to modified Trie (26 ways). Significant improvement (100 points)

--- src/BoggleSolver.java
+++ src/BoggleSolver.java
@@ -65,21 +65,95 @@ public class BoggleSolver
         }
     }
 
-    // modified TST
-    private static class TST<Value> {
-        private int n;              // size
-        private Node<Value> root;   // root of TST
+    // modified Trie
+    private static class TrieSTLite {
+        private static final int R = 26;        // extended ASCII
 
-        private static class Node<Value> {
-            private char c;                        // character
-            private Node<Value> left, mid, right;  // left, middle, and right subtries
-            private Value val;                     // value associated with string
+
+        private Node root;      // root of trie
+        private int n;          // number of keys in trie
+
+        // R-way trie node
+        private static class Node {
+            private Integer val;
+            private Node[] next = new Node[R];
         }
 
         /**
          * Initializes an empty string symbol table.
          */
-        public TST() {
+        public TrieSTLite() {
+        }
+
+        private int resolveIdx(char c){
+            return c - 'A';
+        }
+
+        public boolean containWithPrefix(String prefix) {
+            if (prefix == null) {
+                throw new IllegalArgumentException("calls keysWithPrefix() with null argument");
+            }
+            Node x = get(root, prefix, 0);
+            return x != null;
+        }
+
+        /**
+         * Returns the value associated with the given key.
+         * @param key the key
+         * @return the value associated with the given key if the key is in the symbol table
+         *     and {@code null} if the key is not in the symbol table
+         * @throws IllegalArgumentException if {@code key} is {@code null}
+         */
+        public Integer get(String key) {
+            if (key == null) throw new IllegalArgumentException("argument to get() is null");
+            Node x = get(root, key, 0);
+            if (x == null) return null;
+            return  x.val;
+        }
+
+        /**
+         * Does this symbol table contain the given key?
+         * @param key the key
+         * @return {@code true} if this symbol table contains {@code key} and
+         *     {@code false} otherwise
+         * @throws IllegalArgumentException if {@code key} is {@code null}
+         */
+        public boolean contains(String key) {
+            if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+            return get(key) != null;
+        }
+
+        private Node get(Node x, String key, int d) {
+            if (x == null) return null;
+            if (d == key.length()) return x;
+            char c = key.charAt(d);
+            return get(x.next[resolveIdx(c)], key, d+1);
+        }
+
+        /**
+         * Inserts the key-value pair into the symbol table, overwriting the old value
+         * with the new value if the key is already in the symbol table.
+         * If the value is {@code null}, this effectively deletes the key from the symbol table.
+         * @param key the key
+         * @param val the value
+         * @throws IllegalArgumentException if {@code key} is {@code null}
+         */
+        public void put(String key, Integer val) {
+            if (key == null) throw new IllegalArgumentException("first argument to put() is null");
+            if (val == null) delete(key);
+            else root = put(root, key, val, 0);
+        }
+
+        private Node put(Node x, String key, Integer val, int d) {
+            if (x == null) x = new Node();
+            if (d == key.length()) {
+                if (x.val == null) n++;
+                x.val = val;
+                return x;
+            }
+            char c = key.charAt(d);
+            x.next[resolveIdx(c)] = put(x.next[resolveIdx(c)], key, val, d+1);
+            return x;
         }
 
         /**
@@ -91,75 +165,79 @@ public class BoggleSolver
         }
 
         /**
-         * Does this symbol table contain the given key?
-         * @param key the key
-         * @return {@code true} if this symbol table contains {@code key} and
-         *     {@code false} otherwise
-         * @throws IllegalArgumentException if {@code key} is {@code null}
+         * Is this symbol table empty?
+         * @return {@code true} if this symbol table is empty and {@code false} otherwise
          */
-        public boolean contains(String key) {
-            if (key == null) {
-                throw new IllegalArgumentException("argument to contains() is null");
-            }
-            return get(key) != null;
+        public boolean isEmpty() {
+            return size() == 0;
         }
 
         /**
-         * Returns the value associated with the given key.
-         * @param key the key
-         * @return the value associated with the given key if the key is in the symbol table
-         *     and {@code null} if the key is not in the symbol table
-         * @throws IllegalArgumentException if {@code key} is {@code null}
+         * Returns all keys in the symbol table as an {@code Iterable}.
+         * To iterate over all of the keys in the symbol table named {@code st},
+         * use the foreach notation: {@code for (Key key : st.keys())}.
+         * @return all keys in the symbol table as an {@code Iterable}
          */
-        public Value get(String key) {
-            if (key == null) {
-                throw new IllegalArgumentException("calls get() with null argument");
-            }
-            if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
-            Node<Value> x = get(root, key, 0);
-            if (x == null) return null;
-            return x.val;
-        }
-
-        // return subtrie corresponding to given key
-        private Node<Value> get(Node<Value> x, String key, int d) {
-            if (x == null) return null;
-            if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
-            char c = key.charAt(d);
-            if      (c < x.c)              return get(x.left,  key, d);
-            else if (c > x.c)              return get(x.right, key, d);
-            else if (d < key.length() - 1) return get(x.mid,   key, d+1);
-            else                           return x;
+        public Iterable<String> keys() {
+            return keysWithPrefix("");
         }
 
         /**
-         * Inserts the key-value pair into the symbol table, overwriting the old value
-         * with the new value if the key is already in the symbol table.
-         * If the value is {@code null}, this effectively deletes the key from the symbol table.
-         * @param key the key
-         * @param val the value
-         * @throws IllegalArgumentException if {@code key} is {@code null}
+         * Returns all of the keys in the set that start with {@code prefix}.
+         * @param prefix the prefix
+         * @return all of the keys in the set that start with {@code prefix},
+         *     as an iterable
          */
-        public void put(String key, Value val) {
-            if (key == null) {
-                throw new IllegalArgumentException("calls put() with null key");
-            }
-            if (!contains(key)) n++;
-            else if(val == null) n--;       // delete existing key
-            root = put(root, key, val, 0);
+        public Iterable<String> keysWithPrefix(String prefix) {
+            Queue<String> results = new Queue<>();
+            Node x = get(root, prefix, 0);
+            collect(x, new StringBuilder(prefix), results);
+            return results;
         }
 
-        private Node<Value> put(Node<Value> x, String key, Value val, int d) {
-            char c = key.charAt(d);
-            if (x == null) {
-                x = new Node<>();
-                x.c = c;
+        private void collect(Node x, StringBuilder prefix, Queue<String> results) {
+            if (x == null) return;
+            if (x.val != null) results.enqueue(prefix.toString());
+            for (char c = 0; c < R; c++) {
+                prefix.append(c);
+                collect(x.next[resolveIdx(c)], prefix, results);
+                prefix.deleteCharAt(prefix.length() - 1);
             }
-            if      (c < x.c)               x.left  = put(x.left,  key, val, d);
-            else if (c > x.c)               x.right = put(x.right, key, val, d);
-            else if (d < key.length() - 1)  x.mid   = put(x.mid,   key, val, d+1);
-            else                            x.val   = val;
-            return x;
+        }
+
+        /**
+         * Returns all of the keys in the symbol table that match {@code pattern},
+         * where . symbol is treated as a wildcard character.
+         * @param pattern the pattern
+         * @return all of the keys in the symbol table that match {@code pattern},
+         *     as an iterable, where . is treated as a wildcard character.
+         */
+        public Iterable<String> keysThatMatch(String pattern) {
+            Queue<String> results = new Queue<>();
+            collect(root, new StringBuilder(), pattern, results);
+            return results;
+        }
+
+        private void collect(Node x, StringBuilder prefix, String pattern, Queue<String> results) {
+            if (x == null) return;
+            int d = prefix.length();
+            if (d == pattern.length() && x.val != null)
+                results.enqueue(prefix.toString());
+            if (d == pattern.length())
+                return;
+            char c = pattern.charAt(d);
+            if (c == '.') {
+                for (char ch = 0; ch < R; ch++) {
+                    prefix.append(ch);
+                    collect(x.next[ch], prefix, pattern, results);
+                    prefix.deleteCharAt(prefix.length() - 1);
+                }
+            }
+            else {
+                prefix.append(c);
+                collect(x.next[resolveIdx(c)], prefix, pattern, results);
+                prefix.deleteCharAt(prefix.length() - 1);
+            }
         }
 
         /**
@@ -171,115 +249,63 @@ public class BoggleSolver
          * @throws IllegalArgumentException if {@code query} is {@code null}
          */
         public String longestPrefixOf(String query) {
-            if (query == null) {
-                throw new IllegalArgumentException("calls longestPrefixOf() with null argument");
-            }
-            if (query.length() == 0) return null;
-            int length = 0;
-            Node<Value> x = root;
-            int i = 0;
-            while (x != null && i < query.length()) {
-                char c = query.charAt(i);
-                if      (c < x.c) x = x.left;
-                else if (c > x.c) x = x.right;
-                else {
-                    i++;
-                    if (x.val != null) length = i;
-                    x = x.mid;
-                }
-            }
-            return query.substring(0, length);
+            if (query == null) throw new IllegalArgumentException("argument to longestPrefixOf() is null");
+            int length = longestPrefixOf(root, query, 0, -1);
+            if (length == -1) return null;
+            else return query.substring(0, length);
+        }
+
+        // returns the length of the longest string key in the subtrie
+        // rooted at x that is a prefix of the query string,
+        // assuming the first d character match and we have already
+        // found a prefix match of given length (-1 if no such match)
+        private int longestPrefixOf(Node x, String query, int d, int length) {
+            if (x == null) return length;
+            if (x.val != null) length = d;
+            if (d == query.length()) return length;
+            char c = query.charAt(d);
+            return longestPrefixOf(x.next[resolveIdx(c)], query, d+1, length);
         }
 
         /**
-         * Returns all keys in the symbol table as an {@code Iterable}.
-         * To iterate over all of the keys in the symbol table named {@code st},
-         * use the foreach notation: {@code for (Key key : st.keys())}.
-         * @return all keys in the symbol table as an {@code Iterable}
+         * Removes the key from the set if the key is present.
+         * @param key the key
+         * @throws IllegalArgumentException if {@code key} is {@code null}
          */
-        public Iterable<String> keys() {
-            Queue<String> queue = new Queue<>();
-            collect(root, new StringBuilder(), queue);
-            return queue;
+        public void delete(String key) {
+            if (key == null) throw new IllegalArgumentException("argument to delete() is null");
+            root = delete(root, key, 0);
         }
 
-
-        public boolean containWithPrefix(String prefix) {
-            if (prefix == null) {
-                throw new IllegalArgumentException("calls keysWithPrefix() with null argument");
+        private Node delete(Node x, String key, int d) {
+            if (x == null) return null;
+            if (d == key.length()) {
+                if (x.val != null) n--;
+                x.val = null;
             }
-            Node<Value> x = get(root, prefix, 0);
-            return x != null;
-        }
-
-        /**
-         * Returns all of the keys in the set that start with {@code prefix}.
-         * @param prefix the prefix
-         * @return all of the keys in the set that start with {@code prefix},
-         *     as an iterable
-         * @throws IllegalArgumentException if {@code prefix} is {@code null}
-         */
-        public Queue<String> keysWithPrefix(String prefix) {
-            if (prefix == null) {
-                throw new IllegalArgumentException("calls keysWithPrefix() with null argument");
+            else {
+                char c = key.charAt(d);
+                x.next[resolveIdx(c)] = delete(x.next[resolveIdx(c)], key, d+1);
             }
-            Queue<String> queue = new Queue<>();
-            Node<Value> x = get(root, prefix, 0);
-            if (x == null) return queue;
-            if (x.val != null) queue.enqueue(prefix);
-            collect(x.mid, new StringBuilder(prefix), queue);
-            return queue;
+
+            // remove subtrie rooted at x if it is completely empty
+            if (x.val != null) return x;
+            for (int c = 0; c < R; c++)
+                if (x.next[c] != null)
+                    return x;
+            return null;
         }
-
-        // all keys in subtrie rooted at x with given prefix
-        private void collect(Node<Value> x, StringBuilder prefix, Queue<String> queue) {
-            if (x == null) return;
-            collect(x.left,  prefix, queue);
-            if (x.val != null) queue.enqueue(prefix.toString() + x.c);
-            collect(x.mid,   prefix.append(x.c), queue);
-            prefix.deleteCharAt(prefix.length() - 1);
-            collect(x.right, prefix, queue);
-        }
-
-
-        /**
-         * Returns all of the keys in the symbol table that match {@code pattern},
-         * where . symbol is treated as a wildcard character.
-         * @param pattern the pattern
-         * @return all of the keys in the symbol table that match {@code pattern},
-         *     as an iterable, where . is treated as a wildcard character.
-         */
-        public Iterable<String> keysThatMatch(String pattern) {
-            Queue<String> queue = new Queue<>();
-            collect(root, new StringBuilder(), 0, pattern, queue);
-            return queue;
-        }
-
-        private void collect(Node<Value> x, StringBuilder prefix, int i, String pattern, Queue<String> queue) {
-            if (x == null) return;
-            char c = pattern.charAt(i);
-            if (c == '.' || c < x.c) collect(x.left, prefix, i, pattern, queue);
-            if (c == '.' || c == x.c) {
-                if (i == pattern.length() - 1 && x.val != null) queue.enqueue(prefix.toString() + x.c);
-                if (i < pattern.length() - 1) {
-                    collect(x.mid, prefix.append(x.c), i+1, pattern, queue);
-                    prefix.deleteCharAt(prefix.length() - 1);
-                }
-            }
-            if (c == '.' || c > x.c) collect(x.right, prefix, i, pattern, queue);
-        }
-
 
     }
 
 
-    private final TST<Integer> dictionary;
+    private final TrieSTLite dictionary;
 
 
     // Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
     public BoggleSolver(String[] dictionary){
-        this.dictionary = new TST<>();
+        this.dictionary = new TrieSTLite();
         for (String s: dictionary) this.dictionary.put(s, score(s));
     }
 
